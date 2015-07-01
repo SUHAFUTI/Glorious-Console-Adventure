1e1a5ec3f63a23bc493646a50dac3d3ff1e9c884
Imran Yousifi <imay@dr.dk>
2015-07-01 14:37:52 +0200

    Moved MapSymbols to Dictionary

---
+++ src/GloriousConsoleAdventure/Rendering.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GloriousConsoleAdventure.Enums;
+
+namespace GloriousConsoleAdventure
+{
+    public static class Rendering
+    {
+                    
+            
+
+        public static Dictionary<Block, string> MapSymbols = new Dictionary<Block, string>
+        {
+            {Block.EmptySpace, " "},
+            {Block.Wall, "█"}, //alt + 219 = █
+            {Block.Coin, "ò"}
+        };
+
+    }
+}
