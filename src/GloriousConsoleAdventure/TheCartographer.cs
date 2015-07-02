c18bd57e028722f0d0c75231eb88a322fa4e517e
Imran Yousifi <imay@dr.dk>
2015-07-02 10:24:05 +0200

    moving to next map stuff

---
+++ src/GloriousConsoleAdventure/TheCartographer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloriousConsoleAdventure
+{
+    public static class TheCartographer
+    {
+        public static void DrawThisMapPlease(MapHandler map)
+        {
+            map.MakeCaverns();
+            map.PlaceRandomCoin();
+            map.PrintMap();
+        }
+    }
+}
