e274abaad132b875a6cdefff4ea5b1d3cc02450a
Michael von der Fehr <michael@vonderfehr.dk>
2015-07-02 14:35:43 +0200

    minor refactor

--- src/GloriousConsoleAdventure/Mapping/TheCartographer.cs
+++ src/GloriousConsoleAdventure/Mapping/TheCartographer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GloriousConsoleAdventure
+﻿namespace GloriousConsoleAdventure.Mapping
 {
     public static class TheCartographer
     {
