93fe04b153c2f30efccb6ed25c4f03d5902b483a
Michael von der Fehr <michael@vonderfehr.dk>
2015-07-04 14:55:13 +0200

    cleanup

--- src/GloriousConsoleAdventure/Program.cs
+++ src/GloriousConsoleAdventure/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using GloriousConsoleAdventure.Enums;
 using GloriousConsoleAdventure.Mapping;
+using GloriousConsoleAdventure.Menu;
 using GloriousConsoleAdventure.Models.Hero;
 
 namespace GloriousConsoleAdventure
