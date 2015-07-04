829b368a0906399ef7bf08620990826624fd4acd
Michael von der Fehr <michael@vonderfehr.dk>
2015-07-04 14:50:45 +0200

    abstracted common traits out and added mob class

---
+++ src/GloriousConsoleAdventure/Models/Mob.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GloriousConsoleAdventure.Mapping;
+
+namespace GloriousConsoleAdventure.Models
+{
+    public class Mob
+    {
+        public string Name { get; set; }
+        public Coordinate Coordinates { get; set; }
+    }
+}
